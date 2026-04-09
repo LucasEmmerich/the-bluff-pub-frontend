d1cc8ec0bbc4d8c3a0619763ba794751d2c92e4d
Lucas Araujo Emmerich <laemmerich@gmail.com>
2026-04-09 02:16:23 -0300

    docker 3

--- socket.ts
+++ socket.ts
@@ -4,12 +4,9 @@ import { io } from "socket.io-client";
 export const state = reactive({
   connected: false
 });
-const URL = import.meta.env.NUXT_PUBLIC_API_URL ?? 'http://localhost:3001'
 
-export const socket = io(URL);
-socket.on("connect", () => {
-  state.connected = true;
-});
-socket.on("disconnect", () => {
-  state.connected = false;
-});
+const url = import.meta.env.NUXT_PUBLIC_API_URL ?? 'http://localhost:3001';
+export const socket = io(url);
+
+socket.on("connect", () => { state.connected = true; });
+socket.on("disconnect", () => { state.connected = false; });
